9a37c61c8b6a2cf53e7169623549c8120d8c12d1
williamjparra <w3webservices.net@gmail.com>
2023-07-01 12:19:40 -0400

    interface and model for Order added

---
+++ src/interfaces/Order.interface.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import { Product } from "./Products.interface";
+import { Client } from "./Client.interface";
+
+enum Status {
+	status1 = 0,
+	status2 = 1,
+	status3 = 2,
+}
+
+export interface ProductOrder extends mongoose.Document {
+	product: Product | string;
+	price: number;
+}
+
+export interface Order extends mongoose.Document {
+	products: ProductOrder[];
+	client: Client | string;
+	date: Date;
+	shippingDate: Date;
+	orderTotal: number;
+	iva: number;
+	orderBase: number;
+	status: Status;
+	shippingAddress: string;
+}
---
+++ src/models/Order.Model.ts
@@ -0,0 +1,70 @@
+import { Order, ProductOrder } from "../interfaces/Order.interface";
+import { Schema, model } from "mongoose";
+import ProductModel from "./Products.Model";
+import ClientModel from "./Client.Model";
+
+const ProductSaleSchema = new Schema<ProductOrder>(
+	{
+		product: {
+			type: Schema.Types.ObjectId,
+			ref: ProductModel,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+	},
+	{
+		timestamps: false,
+		versionKey: false,
+	},
+);
+
+const OrderSchema = new Schema<Order>(
+	{
+		products: [ProductSaleSchema],
+		client: {
+			type: Schema.Types.ObjectId,
+			ref: ClientModel,
+			required: true,
+		},
+		date: {
+			type: Date,
+			required: true,
+		},
+		shippingDate: {
+			type: Date,
+			required: true,
+		},
+		orderTotal: {
+			type: Number,
+			required: true,
+		},
+		iva: {
+			type: Number,
+			required: true,
+		},
+		orderBase: {
+			type: Number,
+			required: true,
+		},
+		status: {
+			type: Number,
+			enum: [0, 1, 2],
+			default: 0,
+		},
+		shippingAddress: {
+			type: String,
+			required: true,
+			max: 120,
+		},
+	},
+	{
+		timestamps: true,
+		versionKey: true,
+	},
+);
+
+const OrderModel = model("Orders", OrderSchema);
+export default OrderModel;

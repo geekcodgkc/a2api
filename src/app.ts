424c2ecb0dfcdcf56ec5798b5fefddd2f0c560b0
williamjparra <w3webservices.net@gmail.com>
2023-07-05 09:46:48 -0400

    morgan logger added

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,18 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { router } from "./routes";
 import dbConnect from "./config/mongo";
+import morgan from "morgan";
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(morgan("tiny"));
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 app.use(cookieParser());
+app.use(express.static("public"));
 
 dbConnect()
 	.then(() => console.log("db connected"))

08d21789872c36a4b926ab97b7db9eeae45736f5
williamjparra <w3webservices.net@gmail.com>
2023-07-17 15:45:31 -0400

    nuevas presentaciones

--- src/interfaces/Products.interface.ts
+++ src/interfaces/Products.interface.ts
@@ -14,6 +14,16 @@ enum presentations {
 	paila = "paila",
 	botella = "botella",
 	granel = "granel",
+	CAJA05KG = "CAJA 05KG",
+	CAJA10KG = "CAJA 10KG",
+	CAJA15KG = "CAJA 15KG",
+	CAJA24X200GR = "CAJA 24X200GR",
+	CAJA24X250GR = "CAJA 24X250GR",
+	BULTO12X1LT = "BULTO 12X1LT",
+	BULTO24X500ML = "BULTO 24X500ML",
+	BULTO12X850ML = "BULTO 12X850ML",
+	PAILA18LT = "PAILA 18LT",
+	PAILA15LT = "PAILA 15LT",
 }
 
 export interface Product extends mongoose.Document {
